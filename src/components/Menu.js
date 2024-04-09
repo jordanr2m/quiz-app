20f813db367c2b344258de72beb1c1d3276ca4c2
Jordan J <jordanr2m@gmail.com>
2024-04-09 15:41:01 -0700

    Initial commit.

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function Menu() {
+
+    return (
+        <div className="Menu">
+            <input type="text" placeholder="Name"/>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
+
+export default Menu;
